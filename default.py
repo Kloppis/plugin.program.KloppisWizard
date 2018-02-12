54fc0785675441138e944b53b1dbaa21b2cea90b
MartinLandgraf <36426328+MartinLandgraf@users.noreply.github.com>
2018-02-13 00:27:48 +0100

    Add files via upload

---
+++ default.py
@@ -0,0 +1,155 @@
+import xbmc, xbmcaddon, xbmcgui, xbmcplugin,os,sys
+import shutil
+import urllib2,urllib
+import re
+import extract
+import time
+import downloader
+import plugintools
+import zipfile
+import ntpath
+
+
+USER_AGENT = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
+base='http://kodi.die-kloppis.de'
+ADDON=xbmcaddon.Addon(id='plugin.program.KloppisWizard')
+dialog = xbmcgui.Dialog()
+VERSION = "0.5.0"
+PATH = "KloppisWizard"         
+
+    
+def CATEGORIES():
+    link = OPEN_URL('http://kodi.die-kloppis.de/build/wizard.html').replace('\n','').replace('\r','')
+    match = re.compile('name="(.+?)".+?rl="(.+?)".+?mg="(.+?)".+?anart="(.+?)".+?escription="(.+?)"').findall(link)
+    for name,url,iconimage,fanart,description in match:
+        addDir(name,url,1,iconimage,fanart,description)
+    setView('movies', 'MAIN')
+        
+    
+def OPEN_URL(url):
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    response = urllib2.urlopen(req)
+    link=response.read()
+    response.close()
+    return link
+    
+    
+def wizard(name,url,description):
+    path = xbmc.translatePath(os.path.join('special://home/addons','packages'))
+    dp = xbmcgui.DialogProgress()
+    dp.create("[COLOR=aqua]Kloppis Wizard[/COLOR]","[COLOR=aqua]Lade "+name+" vom Server ...[/COLOR]",'', '[COLOR=red]Bitte warten ...[/COLOR]')
+    lib=os.path.join(path, name+'.zip')
+    try:
+       os.remove(lib)
+    except:
+       pass
+    downloader.download(url, lib, dp)
+    addonfolder = xbmc.translatePath(os.path.join('special://','home'))
+    time.sleep(2)
+    dp.update(0,"[COLOR=aqua]Installiere "+name+" ...[/COLOR]")
+    print '======================================='
+    print addonfolder
+    print '======================================='
+    extract.all(lib,addonfolder,dp)
+    killxbmc()
+
+
+
+def killxbmc():
+    dialog.ok('[COLOR=aqua]Installation beendet![/COLOR]', 'Der Wizard wird nun versuchen Kodi zu beenden', 'Danach bitte Kodi neu starten.', '')
+    os._exit(0)
+    dialog.ok('[COLOR=red]FEHLGESCHLAGEN![/COLOR]', '[COLOR=red]Beenden fehlgeschlagen![/COLOR]', 'Bitte Strom trennen oder Kodi mit einem Taskmanager hart beenden!', '[COLOR=red]AUF KEINEN FALL[/COLOR] Kodi normal beenden!')
+
+
+
+def addDir(name,url,mode,iconimage,fanart,description):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&iconimage="+urllib.quote_plus(iconimage)+"&fanart="+urllib.quote_plus(fanart)+"&description="+urllib.quote_plus(description)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, "Plot": description } )
+        liz.setProperty( "Fanart_Image", fanart )
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
+        return ok
+
+
+
+def get_params():
+        param=[]
+        paramstring=sys.argv[2]
+        if len(paramstring)>=2:
+                params=sys.argv[2]
+                cleanedparams=params.replace('?','')
+                if (params[len(params)-1]=='/'):
+                        params=params[0:len(params)-2]
+                pairsofparams=cleanedparams.split('&')
+                param={}
+                for i in range(len(pairsofparams)):
+                        splitparams={}
+                        splitparams=pairsofparams[i].split('=')
+                        if (len(splitparams))==2:
+                                param[splitparams[0]]=splitparams[1]
+                                
+        return param
+        
+                      
+params=get_params()
+url=None
+name=None
+mode=None
+iconimage=None
+fanart=None
+description=None
+
+
+try:
+        url=urllib.unquote_plus(params["url"])
+except:
+        pass
+try:
+        name=urllib.unquote_plus(params["name"])
+except:
+        pass
+try:
+        iconimage=urllib.unquote_plus(params["iconimage"])
+except:
+        pass
+try:        
+        mode=int(params["mode"])
+except:
+        pass
+try:        
+        fanart=urllib.unquote_plus(params["fanart"])
+except:
+        pass
+try:        
+        description=urllib.unquote_plus(params["description"])
+except:
+        pass
+        
+        
+print str(PATH)+': '+str(VERSION)
+print "Mode: "+str(mode)
+print "URL: "+str(url)
+print "Name: "+str(name)
+print "IconImage: "+str(iconimage)
+
+
+def setView(content, viewType):
+    # set content type so library shows more views and info
+    if content:
+        xbmcplugin.setContent(int(sys.argv[1]), content)
+    if ADDON.getSetting('auto-view')=='true':
+        xbmc.executebuiltin("Container.SetViewMode(%s)" % ADDON.getSetting(viewType) )
+        
+        
+if mode==None or url==None or len(url)<1:
+        CATEGORIES()
+       
+elif mode==1:
+        wizard(name,url,description)
+        
+
+        
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
